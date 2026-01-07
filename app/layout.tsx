227b626b9e6b5fc3e59de025290ed9613a50b1f7
79gun79 <79gun79@gmail.com>
2026-01-07 14:00:23 +0900

    [fix] - 구조 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,7 @@
 import Providers from "@/providers/providers";
 import localFont from "next/font/local";
 import "@/css/globals.css";
-import "@/css/font.css";
-import "@/css/color.css";
-import "@/css/button.css";
-import "@/css/shadow.css";
+
 import Header from "@/components/common/header/Header";
 
 const pretendard = localFont({
