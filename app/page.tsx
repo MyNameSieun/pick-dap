f424b6d32bc58659078735564bc01f7472765e87
79gun79 <79gun79@gmail.com>
2026-01-08 17:18:18 +0900

    [fix] - 버튼 크기 조정

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
+import { Button } from "@/components/ui/button/Button";
+import { Search } from "lucide-react";
+
 const MainPage = () => {
-  return <div className="mx-auto min-w-2xs flex-col"></div>;
+  return (
+    <div className="mx-auto max-w-2xl min-w-2xs flex-col">
+      <div className="flex w-150 flex-col">
+        <Button variant="default" size="lg">
+          버튼
+        </Button>
+        <Button variant="default" size="icon">
+          <Search />
+        </Button>
+        <Button variant="default" size="default">
+          <Search />
+          검색
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
--- components/ui/button/Button.tsx
+++ components/ui/button/Button.tsx
@@ -12,7 +12,7 @@ const buttonVariants = cva(
         default:
           "bg-main-400 text-white hover:bg-main-400/80 active:border active:border-main-400 active:bg-bg-light active:text-main-400",
         white:
-          "text-icon-default active:border-highlight-deep active:bg-highlight-light active:text-highlight-deep border border-b-2 border-gray-300 bg-bg-light shadow-xs hover:bg-bg-deep hover:text-gray-600 dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
+          "text-icon-default active:border-highlight-deep active:bg-highlight-light active:text-highlight-deep border border-2 border-gray-300 bg-bg-light shadow-xs hover:bg-bg-deep hover:text-gray-600 dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         none: "text-gray-1000 border border-2 border-gray-300 hover:bg-bg-deep active:text-highlight-deep active:border-highlight-deep",
         ghost:
           "hover:bg-accent text-icon-default hover:text-gray-1000 dark:hover:bg-accent/50",
@@ -22,8 +22,8 @@ const buttonVariants = cva(
         default: "button-md h-12 px-4 py-2 has-[>svg]:px-3",
         xs: "c1 h-8.5 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5 w-fit font-medium",
         sm: "button-sm h-9 gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "button-lg h-16 px-6 has-[>svg]:px-4 py-7",
-        icon: "size-8 rounded-md p-2.5",
+        lg: "button-lg h-16 px-6 has-[>svg]:px-4 py-5",
+        icon: "size-9 rounded-md",
       },
     },
     defaultVariants: {
