22bf5e0d56cdb8ad927fe3dcc0e2ebaf37670286
79gun79 <79gun79@gmail.com>
2026-01-08 12:58:04 +0900

    [feat] - 헤더 디자인 수정 및 마이페이지 바 구현'

--- components/common/header/Header.tsx
+++ components/common/header/Header.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Button } from "@/components/ui/button/Button";
+import SideMenuBar from "@/features/mypage/components/SideMenuBar";
 import { Bell } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
@@ -9,59 +11,83 @@ import { useState } from "react";
 const Header = () => {
   const pathname = usePathname();
   const [isLogin, setLogin] = useState(false);
+  const [isVisible, setVisible] = useState(false);
 
   return (
     <>
-      <header className="c1 bg-bg-default flex h-17.5 w-full items-center border-b-gray-300 px-10">
-        <div className="relative aspect-[3/2] h-full">
-          <Link href="/">
-            <Image src="/logo.png" fill className="object-fill" alt="로고" />
-          </Link>
-        </div>
-        <div className="flex-1"></div>
-        <div className="flex items-center justify-center gap-10">
-          <Link
-            className={`${pathname.startsWith("/question") ? "font-bold" : "font-normal"}`}
-            href={`/question`}
-          >
-            면접 질문
-          </Link>
-          <Link
-            className={`${pathname.startsWith("/interview") ? "font-bold" : "font-normal"}`}
-            href={`/interview`}
-          >
-            면접 연습
-          </Link>
-          <Link
-            className={`${pathname.startsWith("/review") ? "font-bold" : "font-normal"}`}
-            href={`/review`}
-          >
-            면접 후기
-          </Link>
-          <Link
-            className={`${pathname.startsWith("/community") ? "font-bold" : "font-normal"}`}
-            href={`/community`}
-          >
-            커뮤니티
-          </Link>
-        </div>
-        <div className="flex-1"></div>
-        <div className="flex items-center justify-end gap-8">
-          <Bell className="cursor-pointer" onClick={() => setLogin(!isLogin)} />
-          {isLogin ? (
-            <div className="relative h-12 w-12 overflow-hidden rounded-full">
-              <Image
-                className="object-contain"
-                src="/profile.jpg"
-                fill
-                alt="프로필"
-              />
+      <div className="relative w-full">
+        <header className="c1 bg-bg-light flex h-17.5 items-center border-b border-b-gray-300 px-10">
+          <div className="relative aspect-[3/2] h-full">
+            <Link href="/">
+              <Image src="/logo.png" fill className="object-fill" alt="로고" />
+            </Link>
+          </div>
+          <div className="flex-1"></div>
+          <div className="text-gray-1000 flex items-center justify-center gap-10">
+            <Link
+              className={`${pathname.startsWith("/question") ? "font-bold" : "font-normal"}`}
+              href={`/question`}
+            >
+              면접 질문
+            </Link>
+            <Link
+              className={`${pathname.startsWith("/interview") ? "font-bold" : "font-normal"}`}
+              href={`/interview`}
+            >
+              면접 연습
+            </Link>
+            <Link
+              className={`${pathname.startsWith("/review") ? "font-bold" : "font-normal"}`}
+              href={`/review`}
+            >
+              면접 후기
+            </Link>
+            <Link
+              className={`${pathname.startsWith("/community") ? "font-bold" : "font-normal"}`}
+              href={`/community`}
+            >
+              커뮤니티
+            </Link>
+          </div>
+          <div className="flex-1"></div>
+          <div className="flex w-1/6 min-w-40 flex-none justify-center">
+            {isLogin ? (
+              <div className="flex items-center gap-8">
+                <Bell
+                  className="text-icon-default cursor-pointer"
+                  onClick={() => setLogin(!isLogin)}
+                />
+                <div
+                  onClick={() => setVisible(!isVisible)}
+                  className="relative h-12 w-12 cursor-pointer overflow-hidden rounded-full"
+                >
+                  <Image
+                    className="object-contain"
+                    src="/profile.jpg"
+                    fill
+                    alt="프로필"
+                  />
+                </div>
+              </div>
+            ) : (
+              <div className="flex items-center gap-4">
+                <Button onClick={() => setLogin(!isLogin)}>로그인</Button>
+                <Button>회원가입</Button>
+              </div>
+            )}
+          </div>
+        </header>
+        {isVisible && (
+          <div className="absolute top-21 right-10 z-50 h-fit w-fit rounded-xl border border-gray-300 p-4 shadow-md">
+            <SideMenuBar />
+            <div className="-mx-4 -mb-4">
+              <Button className="text-tag-text-gray h-11 w-full bg-gray-100 text-xs">
+                로그아웃
+              </Button>
             </div>
-          ) : (
-            <></>
-          )}
-        </div>
-      </header>
+          </div>
+        )}
+      </div>
     </>
   );
 };
