b66a96d791d6c7059b612d2d8f6a5ae8ee3a3264
79gun79 <79gun79@gmail.com>
2026-01-10 19:46:33 +0900

    [feat] - EmptyStateBox 정리

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-import InfoAuthor from '@/components/common/Info/InfoAuthor';
-
 const MainPage = () => {
-  return (
-    <div className="mx-auto max-w-2xl min-w-2xs flex-col">
-      <InfoAuthor />
-    </div>
-  );
+  return <div className="mx-auto max-w-2xl min-w-2xs flex-col"></div>;
 };
 
 export default MainPage;
