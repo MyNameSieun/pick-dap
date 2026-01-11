1f5168e6b126c62758ca21f5f3f0d80ba0509236
박시은 <charm516@naver.com>
2026-01-11 21:34:27 +0900

    [feat] - Publish Community Tap in Mypage

--- app/mypage/rooms/page.tsx
+++ app/mypage/rooms/page.tsx
@@ -1,10 +1,22 @@
-import AiRecodeSelected from '@/features/mypage/components/AiRecodeSelected';
+'use client';
+import ListSelectionHeader from '@/features/mypage/components/ListSelectionHeader';
 import AiRoomCard from '@/features/mypage/components/AiRoomCard';
+import { FolderOpen } from 'lucide-react';
 
 const MypageRoomsPage = () => {
+  const handleDeleteCard = () => {
+    alert('삭제');
+  };
   return (
     <div>
-      <AiRecodeSelected />
+      <ListSelectionHeader
+        title="픽봇 AI 면접"
+        icon={FolderOpen}
+        content="AI 면접관과의 대화 기록을 확인하고 복습하세요"
+        onAction={handleDeleteCard}
+        selectedCount={2}
+        actionButtonName="선택한 항목 삭제"
+      />
       <div className="container-col gap-2.5">
         <AiRoomCard />
         <AiRoomCard />
