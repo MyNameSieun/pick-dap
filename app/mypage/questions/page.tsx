213d2e140bacaebb7b529af6b8e1181f1486461e
박시은 <charm516@naver.com>
2026-01-11 12:13:57 +0900

    [refactor] - SideMenuBar 메뉴 항목 상수화 및 map 적용

--- app/mypage/questions/page.tsx
+++ app/mypage/questions/page.tsx
@@ -1,5 +1,43 @@
+import Line from '@/components/common/Line';
+import TagSearchBar from '@/components/TagSearchBar';
+import { Bookmark, Bot, FolderOpen, Settings, Users } from 'lucide-react';
+
+import profileImage from '@/public/profile.jpg';
+import Image from 'next/image';
+
 const MypageQuestionsPage = () => {
-  return <div>MypageQuestionsPage</div>;
+  return (
+    <main className="flex gap-4">
+      <section className="w-[238] rounded-md border border-gray-300 bg-white p-6 shadow-md">
+        <article className="flex flex-col items-center gap-5">
+          <div className="relative h-30 w-30 overflow-hidden rounded-full">
+            <Image
+              className="object-cover"
+              alt="작성자 프로필"
+              src={profileImage}
+              fill
+              priority
+            />
+          </div>
+          <div className="flex gap-3">
+            <h6 className="text-gray-800">사용자2</h6>
+            <Settings className="text-icon-default" />
+          </div>
+        </article>
+        <Line />
+
+        <article>
+          <h6>마이페이지</h6>
+        </article>
+
+        <Line />
+      </section>
+
+      <section className="flex-1">
+        <TagSearchBar />
+      </section>
+    </main>
+  );
 };
 
 export default MypageQuestionsPage;
--- features/mypage/components/SideMenuBar.tsx
+++ features/mypage/components/SideMenuBar.tsx
@@ -1,39 +1,54 @@
-"use client";
+import { Bookmark, Bot, FolderOpen, Settings, Users } from 'lucide-react';
+import Link from 'next/link';
 
-import { Bookmark, Bot, FolderOpen, Settings, Users } from "lucide-react";
+const menuList = [
+  {
+    link: '/mypage/question',
+    name: '저장된 질문',
+    icon: Bookmark,
+  },
+  {
+    link: '/mypage/rooms',
+    name: '픽봇 AI 면접',
+    icon: Bot,
+  },
+  {
+    link: '/mypage/community',
+    name: '커뮤니티',
+    icon: Users,
+  },
+  {
+    link: '/mypage/projects',
+    name: '프로젝트',
+    icon: FolderOpen,
+  },
+];
 
 const SideMenuBar = () => {
   return (
-    <>
-      <aside className="flex h-fit w-38.5 flex-col">
-        <div className="ditems-center flex h-8 w-full justify-between border-b border-b-gray-300 pb-2">
-          <p className="text-button-sm font-bold text-gray-800">마이페이지</p>
-          <Settings
-            className="text-icon-default hover:text-gray-1000 cursor-pointer"
-            height={20}
-            width={20}
-          />
-        </div>
-        <div className="flex h-fit w-full cursor-pointer flex-col gap-2 py-4">
-          <div className="flex h-7 w-full items-center gap-5 rounded-sm px-2.5 py-1.5 hover:bg-gray-100">
-            <Bookmark className="text-icon-default" width={20} height={20} />
-            <p className="text-button-sm text-gray-800">저장된 질문</p>
-          </div>
-          <div className="flex h-7 w-full items-center gap-5 rounded-sm px-2.5 py-1.5 hover:bg-gray-100">
-            <Bot className="text-icon-default" width={20} height={20} />
-            <p className="text-button-sm text-gray-800">픽봇 AI 면접</p>
-          </div>
-          <div className="flex h-7 w-full items-center gap-5 rounded-sm px-2.5 py-1.5 hover:bg-gray-100">
-            <Users className="text-icon-default" width={20} height={20} />
-            <p className="text-button-sm text-gray-800">커뮤니티</p>
-          </div>
-          <div className="flex h-7 w-full items-center gap-5 rounded-sm px-2.5 py-1.5 hover:bg-gray-100">
-            <FolderOpen className="text-icon-default" width={20} height={20} />
-            <p className="text-button-sm text-gray-800">프로젝트</p>
-          </div>
-        </div>
-      </aside>
-    </>
+    <aside className="flex h-fit w-38.5 flex-col">
+      <div className="ditems-center flex h-8 w-full justify-between border-b border-b-gray-300 pb-2">
+        <p className="text-button-sm font-bold text-gray-800">
+          <Link href={'/mypage'}>마이페이지</Link>
+        </p>
+        <Settings
+          className="text-icon-default hover:text-gray-1000 cursor-pointer"
+          height={20}
+          width={20}
+        />
+      </div>
+
+      <nav className="flex h-fit w-full cursor-pointer flex-col gap-2 py-4">
+        {menuList.map((menu) => (
+          <Link href={menu.link} key={menu.link}>
+            <div className="flex h-7 w-full items-center gap-5 rounded-sm px-2.5 py-1.5 hover:bg-gray-100">
+              <menu.icon className="text-icon-default" width={20} height={20} />
+              <p className="text-button-sm text-gray-800">{menu.name}</p>
+            </div>
+          </Link>
+        ))}
+      </nav>
+    </aside>
   );
 };
 export default SideMenuBar;
