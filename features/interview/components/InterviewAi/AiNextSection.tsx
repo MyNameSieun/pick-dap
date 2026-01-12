c247e80391128dd5a30419db98675f084278fc16
79gun79 <79gun79@gmail.com>
2026-01-12 18:32:45 +0900

    [fix] - pr 반영하여 컴포넌트 구조 변경

--- features/interview/components/InterviewAi/AiNextSection.tsx
+++ features/interview/components/InterviewAi/AiNextSection.tsx
@@ -1,23 +1,33 @@
-import SelectCountBox from '@/components/common/SelectCountBox/SelectCountBox';
+'use client';
+
 import { Lightbulb } from 'lucide-react';
 import { twMerge } from 'tailwind-merge';
 import { Button } from '@/components/ui/button/Button';
 import AiRoomCard from '@/components/common/AiRoomCard';
+import { useState } from 'react';
 
 const AiNextSection = () => {
+  const [isEditMode, setIsEditMode] = useState(false);
+
+  const handleChooseButton = () => {
+    setIsEditMode(!isEditMode);
+  };
+
   return (
     <>
       <div className={twMerge('container-col', 'w-3/5 gap-8 p-9')}>
         <div className="flex items-start justify-between">
           <h6 className="h6 text-black">생성된 질문 (10개)</h6>
-          <Button variant="white" className="h-9.5 font-bold">
+          <Button
+            variant="white"
+            className="h-9.5 font-bold"
+            onClick={handleChooseButton}
+          >
             질문 담기
           </Button>
         </div>
         <div className="flex flex-col gap-3">
-          <SelectCountBox count={0} state="save" className="min-w-full" />
-          <AiRoomCard />
-          <AiRoomCard />
+          <AiRoomCard isEditMode={isEditMode} setIsEditMode={setIsEditMode} />
         </div>
         <div className="my-4 flex flex-col items-center justify-center gap-5.5 text-gray-500">
           <Lightbulb size={100} />
