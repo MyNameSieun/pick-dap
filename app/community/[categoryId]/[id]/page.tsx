66e657c8ccaf9cad264d50619680d25d491c1673
79gun79 <79gun79@gmail.com>
2026-01-13 19:23:49 +0900

    [feat] - 커뮤니티 상세 페이지 메타데이터 추가

--- app/community/[categoryId]/[id]/page.tsx
+++ app/community/[categoryId]/[id]/page.tsx
@@ -1,5 +1,16 @@
+interface Props {
+  params: Promise<{ categoryId: string; id: string }>;
+}
+
+export const generateMetadata = async ({ params }: Props) => {
+  const { categoryId, id } = await params;
+  return {
+    title: `커뮤니티 ${categoryId}-${id}`,
+  };
+};
+
 const CommunityCategoryDetailIdPage = () => {
-  return <div>CommunityCategoryDetailPage</div>;
+  return <></>;
 };
 
 export default CommunityCategoryDetailIdPage;
