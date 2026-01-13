3b2392b5663be3d73d253292526a7cf0011d0109
박시은 <charm516@naver.com>
2026-01-13 11:11:20 +0900

    [refactor] - HeaderTitleBox props 구조 변경

--- app/review/page.tsx
+++ app/review/page.tsx
@@ -1,5 +1,25 @@
+'use client';
+import HeaderTitleBox from '@/components/common/HeaderTitleBox';
+import { FolderOpen } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+
 const ReviewPage = () => {
-  return <div>ReviewPage</div>;
+  const router = useRouter();
+  return (
+    <div>
+      <HeaderTitleBox
+        title={'면접 후기'}
+        content={'실제 면접 경험을 공유하고 다른사람들의 후기를 확인해 보세요 '}
+        icon={FolderOpen}
+        buttonOption={{
+          text: '후기 등록',
+          action: () => {
+            router.push('/');
+          },
+        }}
+      />
+    </div>
+  );
 };
 
 export default ReviewPage;
--- components/common/HeaderTitleBox.tsx
+++ components/common/HeaderTitleBox.tsx
@@ -1,33 +1,58 @@
 import { LucideIcon } from 'lucide-react';
+import { Button } from '../ui/button/Button';
 
 interface HeaderTitleBoxProps {
   title?: React.ReactNode;
   content?: React.ReactNode;
   icon: LucideIcon;
-  bgColor?: string;
-  iconColor?: string;
-  titleColor?: string;
-  subColor?: string;
+
+  colorOption?: {
+    bgColor?: string;
+    iconColor?: string;
+    titleColor?: string;
+    subColor?: string;
+  };
+
+  buttonOption?: {
+    text: string;
+    action?: () => void;
+    variant?: 'default' | 'white';
+  };
 }
 
 const HeaderTitleBox = ({
   title,
   content,
   icon: Icon,
-  bgColor = 'bg-blue-100',
-  iconColor = 'text-blue-400',
-  titleColor = 'text-gray-1000',
-  subColor = 'text-gray-700',
+  colorOption: {
+    bgColor = 'bg-blue-100',
+    iconColor = 'text-blue-400',
+    titleColor = 'text-gray-1000',
+    subColor = 'text-gray-700',
+  } = {},
+
+  buttonOption,
 }: HeaderTitleBoxProps) => {
   return (
-    <div className="flex gap-4">
-      <div className={`rounded-[5px] ${bgColor} p-4.5 shadow-sm`}>
-        <Icon className={`${iconColor}`} />
+    <div className="flex justify-between">
+      <div className="flex gap-4">
+        <div className={`rounded-[5px] ${bgColor} p-4.5 shadow-sm`}>
+          <Icon className={`${iconColor}`} />
+        </div>
+        <div className="flex flex-col justify-around">
+          <div className={`text-[20px] font-bold ${titleColor}`}>{title}</div>
+          <div className={`c1 ${subColor}`}>{content}</div>
+        </div>
       </div>
-      <div className="flex flex-col justify-around">
-        <div className={`text-[20px] font-bold ${titleColor}`}>{title}</div>
-        <div className={`c1 ${subColor}`}>{content}</div>
-      </div>
+
+      {buttonOption && (
+        <Button
+          onClick={buttonOption.action}
+          variant={buttonOption.variant || 'default'}
+        >
+          {buttonOption.text}
+        </Button>
+      )}
     </div>
   );
 };
