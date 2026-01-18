37c09bd587eb768bc5189dc5c38dfb414c05d0a8
79gun79 <79gun79@gmail.com>
2026-01-18 19:09:04 +0900

    [feat] - 게시판 퍼블리싱 완료

--- features/community/components/CommunityCRUD/CommunityCreate.tsx
+++ features/community/components/CommunityCRUD/CommunityCreate.tsx
@@ -1,17 +1,28 @@
 'use client';
 
 import BackButton from '@/components/common/BackButton';
-import Line from '@/components/common/Line';
 import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
-import Link from '@tiptap/extension-link';
-import { Markdown } from 'tiptap-markdown';
 import { twMerge } from 'tailwind-merge';
 import CommunityToolBar from './CommunityToolBar';
 import { useState } from 'react';
+import Link from '@tiptap/extension-link';
+import { Markdown } from 'tiptap-markdown';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/Select';
+import { communityMenuData } from '@/data/menuData';
+import { Button } from '@/components/ui/button/Button';
 
 const CommunityCreate = () => {
-  const [text, setText] = useState('Hello World!');
+  const [text, setText] = useState('원하는 내용을 작성해보세요!');
+  const displayData = communityMenuData.filter((v) => v.id != '1');
 
   const editor = useEditor({
     extensions: [
@@ -22,11 +33,15 @@ const CommunityCreate = () => {
       Markdown,
     ],
     content: text,
+    editorProps: {
+      attributes: {
+        class: 'min-h-[300px] outline-none',
+      },
+    },
     onUpdate({ editor }) {
       setText(editor.getHTML());
     },
-    shouldRerenderOnTransaction: true,
-    immediatelyRender: true,
+    immediatelyRender: false,
   });
 
   if (!editor) return null;
@@ -37,16 +52,43 @@ const CommunityCreate = () => {
         <BackButton label={<p className="font-bold">뒤로가기</p>} />
       </div>
 
-      <div className="h-9 w-40">게시판 선택</div>
-      <Line my={1} />
-      <CommunityToolBar editor={editor} />
+      <Select>
+        <SelectTrigger className="h-9 w-40 cursor-pointer">
+          <SelectValue placeholder="게시판 선택" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectGroup>
+            <SelectLabel>커뮤니티</SelectLabel>
+            {displayData.map((v) => (
+              <SelectItem className="cursor-pointer" key={v.id} value={v.label}>
+                {v.label}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
 
-      <EditorContent
-        editor={editor}
-        className={twMerge(
-          'b1 text-gray-1000 min-h-100 w-full rounded-[8px] border border-gray-500 p-4',
-        )}
+      <input
+        placeholder="제목을 입력하세요..."
+        className="text-gray-1000 selection:bg-main-400 mb-2 w-full border-b border-b-gray-500 py-2 text-[28px] font-bold outline-0 selection:text-white placeholder:text-gray-600"
       />
+
+      <div className="relative min-h-100 w-full rounded-[8px] border border-gray-500">
+        <CommunityToolBar editor={editor} />
+        <EditorContent
+          editor={editor}
+          className={twMerge(
+            'b1 text-gray-1000 px-4 pt-16 [&_.ProseMirror]:focus:outline-none',
+          )}
+        />
+      </div>
+
+      <div className="flex w-full items-center justify-end gap-5">
+        <Button variant="white" className="h-9 w-25">
+          취소
+        </Button>
+        <Button className="h-9 w-25">저장</Button>
+      </div>
     </>
   );
 };
--- features/community/components/CommunityCRUD/CommunityToolBar.tsx
+++ features/community/components/CommunityCRUD/CommunityToolBar.tsx
@@ -1,6 +1,17 @@
 import { Button } from '@/components/ui/button/Button';
 import { Editor } from '@tiptap/react';
-import { Bold } from 'lucide-react';
+import {
+  Bold,
+  FlipHorizontal,
+  Heading1,
+  Heading2,
+  Heading3,
+  Italic,
+  Link,
+  List,
+  ListOrdered,
+  Strikethrough,
+} from 'lucide-react';
 
 type ToolBarProps = {
   editor: Editor;
@@ -8,12 +19,97 @@ type ToolBarProps = {
 
 const CommunityToolBar = ({ editor }: ToolBarProps) => {
   return (
-    <div className="flex h-12 w-full items-center rounded-[8px] border border-gray-500 p-2">
-      <Button size="icon" variant="ghost">
-        <Bold
-          onClick={() => editor.chain().focus().toggleBold().run()}
-          className={editor.isActive('bold') ? 'is-active' : ''}
+    <div className="absolute top-0 right-0 left-0 flex items-center rounded-[8px] border-b border-b-gray-500 bg-gray-100/50 p-1">
+      <Button
+        size="icon"
+        variant="ghost"
+        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+      >
+        <Heading1
+          className={
+            editor.isActive('heading', { level: 2 }) ? 'is-active' : ''
+          }
         />
+      </Button>
+      <Button
+        size="icon"
+        variant="ghost"
+        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+      >
+        <Heading2
+          className={
+            editor.isActive('heading', { level: 3 }) ? 'is-active' : ''
+          }
+        />
+      </Button>
+      <Button
+        size="icon"
+        variant="ghost"
+        onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
+      >
+        <Heading3
+          className={
+            editor.isActive('heading', { level: 4 }) ? 'is-active' : ''
+          }
+        />
+      </Button>
+
+      <div className="mx-2 h-6 w-[1px] bg-gray-400" />
+
+      <Button
+        size="icon"
+        variant="ghost"
+        onClick={() => editor.chain().focus().toggleBold().run()}
+      >
+        <Bold className={editor.isActive('bold') ? 'is-active' : ''} />
+      </Button>
+      <Button
+        size="icon"
+        variant="ghost"
+        onClick={() => editor.chain().focus().toggleItalic().run()}
+      >
+        <Italic className={editor.isActive('italic') ? 'is-active' : ''} />
+      </Button>
+      <Button
+        size="icon"
+        variant="ghost"
+        onClick={() => editor.chain().focus().toggleStrike().run()}
+      >
+        <Strikethrough
+          className={editor.isActive('strike') ? 'is-active' : ''}
+        />
+      </Button>
+
+      <div className="mx-2 h-6 w-[1px] bg-gray-400" />
+
+      <Button
+        size="icon"
+        variant="ghost"
+        onClick={() => editor.chain().focus().toggleBulletList().run()}
+      >
+        <List className={editor.isActive('bulletList') ? 'is-active' : ''} />
+      </Button>
+      <Button
+        size="icon"
+        variant="ghost"
+        onClick={() => editor.chain().focus().toggleOrderedList().run()}
+      >
+        <ListOrdered
+          className={editor.isActive('orderedList') ? 'is-active' : ''}
+        />
+      </Button>
+
+      <div className="mx-2 h-6 w-[1px] bg-gray-400" />
+
+      <Button size="icon" variant="ghost" onClick={() => {}}>
+        <Link className={editor.isActive('link') ? 'is-active' : ''} />
+      </Button>
+      <Button
+        size="icon"
+        variant="ghost"
+        onClick={() => editor.chain().focus().setHorizontalRule().run()}
+      >
+        <FlipHorizontal />
       </Button>
     </div>
   );
