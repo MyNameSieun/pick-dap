bb69efe902e91516bfff4fa35015ba764e0dac52
sieun <charm516@naver.com>
2026-01-21 18:46:11 +0900

    [feat/#88] - 구글 회원가입 구현

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,5 +1,3 @@
-import { Button } from '@/components/ui/button/Button';
-
 import Link from 'next/link';
 import SocialSignupButton from '@/features/auth/SocialSignupButton';
 import AuthHeader from '@/features/auth/AuthHeader';
